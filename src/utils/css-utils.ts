f592e1a79aa7c306586e88ae6c51f1ca0410f389
anxpara <anxpara@gmail.com>
2022-01-10 05:42:40 +0000

    Add getCenterOfElement and logMat4 css utils

--- src/utils/css-utils.ts
+++ src/utils/css-utils.ts
@@ -10,6 +10,10 @@ export function getOffsetRectOfElement(element: HTMLElement): DOMRect {
   return new DOMRect(element.offsetLeft, element.offsetTop, element.offsetWidth, element.offsetHeight);
 }
 
+export function getCenterOfElement(element: HTMLElement): vec3 {
+  return vec3.fromValues(element.offsetWidth / 2, element.offsetHeight / 2, 0);
+}
+
 export function getTransformOriginOfElement(element: HTMLElement): vec3 {
   let originString = window.getComputedStyle(element).transformOrigin;
   let coords = originString.split(" ").map((str) => Number.parseFloat(str));
@@ -80,4 +84,11 @@ export function convertMat4ToCssTransformString(mat: mat4): string {
   str = str.split("(")[1];
   str = str.split(")")[0];
   return str;
+}
+
+export function logMat4(mat: mat4): void {
+  console.log("%f, %f, %f, %f", mat[0], mat[1], mat[2], mat[3]);
+  console.log("%f, %f, %f, %f", mat[4], mat[5], mat[6], mat[7]);
+  console.log("%f, %f, %f, %f", mat[8], mat[9], mat[10], mat[11]);
+  console.log("%f, %f, %f, %f", mat[12], mat[13], mat[14], mat[15]);
 }
