d06a35e61f7f2b2fa5d08c4654c02ba01ed4040a
anxpara <anxpara@gmail.com>
2022-10-21 21:51:03 +0000

    Trialgrounds--adjust index url

--- trialgrounds/src/index.ts
+++ trialgrounds/src/index.ts
@@ -4,18 +4,18 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/trialgrounds", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../build/trialgrounds.html"));
 });
 
-app.get("/trialgrounds/trialgrounds.css", function (req, res) {
+app.get("/trialgrounds.css", function (req, res) {
   res.sendFile(path.join(__dirname, "../build/trialgrounds.css"));
 });
 
-app.get("/trialgrounds/trialgrounds.js", function (req, res) {
+app.get("/trialgrounds.js", function (req, res) {
   res.sendFile(path.join(__dirname, "../build/trialgrounds.js"));
 });
 
 app.listen(PORT, () => {
-  console.log(`⚡️[Trialgrounds]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[Trialgrounds]: Server is running at http://localhost:${PORT}`);
 });
